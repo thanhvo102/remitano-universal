553ff1155dc6dfddaf66206388330a1b21719042
Thanh Vo <vo.t.thanh.26@gmail.com>
2021-02-04 23:48:06 +0700

    Add next-images plugin

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 
 const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
 const withFonts = require("next-fonts");
+const withImages = require("next-images");
 
-module.exports = withExpo(
-  withFonts({
-    projectRoot: __dirname,
-  })
+module.exports = withPlugins(
+  [
+    withFonts,
+    withImages,
+    [withExpo, { projectRoot: __dirname }],
+  ],
 );
